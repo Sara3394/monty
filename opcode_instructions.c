cc5a05b88364aec39e6d02ea226b8bbb1ef45a76
Sara3394 <hailu.serafina@yahoo.com>
2022-09-08 07:11:24 -0700

    commit

--- opcode_instructions.c
+++ opcode_instructions.c
@@ -1,73 +1,136 @@
 #include "monty.h"
-/**
- * _calloc - concatenate tw strings specially
- * @nmemb: number of elements
- * @size: type of elements
- * Return: nothing
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *p = NULL;
-	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
-	return (p);
-}
 /**
- * _realloc - change the size and copy the content
- * @ptr: malloc pointer to reallocate
- * @old_size: old number of bytes
- * @new_size: new number of Bytes
- * Return: nothing
+ * _push - pushes an element to the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void _push(stack_t **doubly, unsigned int cline)
 {
-	char *p = NULL;
-	unsigned int i;
+	int n, j;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
+	if (!vglo.arg)
 	{
-		p = malloc(new_size);
-		if (!p)
-			return (NULL);
-		return (p);
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
 	}
-	if (new_size == 0 && ptr != NULL)
+
+	for (j = 0; vglo.arg[j] != '\0'; j++)
 	{
-		free(ptr);
-		return (NULL);
+		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
 	}
-	if (new_size > old_size)
-	{
-		p = malloc(new_size);
-		if (!p)
-			return (NULL);
-		for (i = 0; i < old_size; i++)
-			p[i] = *((char *)ptr + i);
-		free(ptr);
-	}
+
+	n = atoi(vglo.arg);
+
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, n);
 	else
+		add_dnodeint_end(doubly, n);
+}
+
+/**
+ * _pall - prints all values on the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line numbers
+ * Return: no return
+ */
+void _pall(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux)
 	{
-		p = malloc(new_size);
-		if (!p)
-			return (NULL);
-		for (i = 0; i < new_size; i++)
-			p[i] = *((char *)ptr + i);
-		free(ptr);
+		printf("%d\n", aux->n);
+		aux = aux->next;
 	}
-	return (p);
+}
+
+/**
+ * _pint - prints the value at the top of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pint(stack_t **doubly, unsigned int cline)
+{
+	(void)cline;
+
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*doubly)->n);
+}
+
+/**
+ * _pop - removes the top element of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pop(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	free(aux);
+}
+
+/**
+ * _swap - swaps the top two elements of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _swap(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
 }
